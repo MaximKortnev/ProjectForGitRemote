e6ccf802409c6dee99e89d8f17a54e16f696e8a8
MaxKor <fafccc@gmail.com>
2023-08-04 21:35:23 +0300

    Выполнена подзадача 3

--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -10,6 +10,7 @@ namespace GitPractic
             // Изменение для подзадачи 1
             Console.WriteLine("Hello World");
             // Изменение для подзадачи 2
+            // Изменение для подзадачи 3
         }
     }
 }
