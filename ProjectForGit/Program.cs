004e2ecafb116f1a69ac2f721cf1db1649756e39
MaxKor <fafccc@gmail.com>
2023-08-05 07:37:14 +0300

    Del other

--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -7,11 +7,8 @@ namespace GitPractic
     {
         static void Main(string[] args)
         {
-            // Изменение для подзадачи 1
             Console.WriteLine("Hello World");
-            // Изменение для подзадачи 2
-            // Изменение для подзадачи 3
-            // Изменение для подзадачи 4
+
         }
     }
 }
